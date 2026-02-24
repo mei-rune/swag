9db44174ed71b04ba095af6224fef981a820b5fe
mfk <mfk@hengwei.com.cn>
2026-02-24 16:54:19 +0800

    fox bugs

--- schema.go
+++ schema.go
@@ -157,6 +157,9 @@ func RefSchema(refType string) *spec.Schema {
 }
 
 func GetRefTypeFromRefSchema(schema *spec.Schema) string {
+	if schema.Ref.GetURL() == nil {
+		return ""
+	}
 	return strings.TrimPrefix(schema.Ref.GetURL().String(), "#/definitions/")
 }
 
